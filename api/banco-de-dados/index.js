3aedea135545f08a7260a6bbba16626ef01ff318
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2022-11-23 19:30:13 -0300

    adicionando pacote eslint no projeto

--- api/banco-de-dados/index.js
+++ api/banco-de-dados/index.js
@@ -2,13 +2,13 @@ const Sequelize = require('sequelize')
 const config = require('../../config/default.json')
 
 const instancia = new Sequelize(
-    config.mysql.banco_de_dados,
-    config.mysql.usuario,
-    config.mysql.senha,
-    {
-        host: config.mysql.host,
-        dialect: 'mysql'
-    }
+  config.mysql.banco_de_dados,
+  config.mysql.usuario,
+  config.mysql.senha,
+  {
+    host: config.mysql.host,
+    dialect: 'mysql'
+  }
 )
 
 module.exports = instancia
