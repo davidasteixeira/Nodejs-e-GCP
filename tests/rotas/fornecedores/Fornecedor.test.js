65798861ba7ec4f840ccd147852f52b56d6a5415
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2022-11-24 21:01:45 -0300

    Adicionando configurações do github actions e atualizando package

--- tests/rotas/fornecedores/Fornecedor.test.js
+++ tests/rotas/fornecedores/Fornecedor.test.js
@@ -1,3 +1,4 @@
+jest.mock('../../../api/rotas/fornecedores/TabelaFornecedor')
 const Fornecedor = require('../../../api/rotas/fornecedores/Fornecedor')
 
 describe('Classe Fornecedor', () => {
@@ -10,4 +11,19 @@ describe('Classe Fornecedor', () => {
 
     expect(fornecedor.validar()).toBe(true)
   })
-});
+
+  it('O método criar() foi executado com sucesso', async () => {
+    const fornecedor = new Fornecedor({
+      empresa: 'Gatito',
+      email: 'contato@gatito.com.br',
+      categoria: 'brinquedos'
+    })
+
+    await fornecedor.criar()
+
+    expect(fornecedor.id).toBe(500)
+    expect(fornecedor.dataCriacao).toBe('10/12/3420')
+    expect(fornecedor.dataAtualizacao).toBe('10/12/3420')
+    expect(fornecedor.versao).toBe(90)
+  })
+})
